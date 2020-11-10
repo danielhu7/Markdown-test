88aee0ad27e8c71961c34499fc8d2f9f3c8b418e
Daniel Hidalgo <hidalgure.daniel@gmail.com>
2020-11-10 16:39:21 -0600

    Agregar docusaurios config

---
+++ docsaurius-test/docusaurus.config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  title: 'Editor GTFS',
+  tagline: 'The tagline of my site',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/Markdown-test/docsaurius-test/build/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Camellito', // Usually your GitHub org/user name.
+  projectName: 'shape editor', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Editor GTFS',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Documentos',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/jeancahu/GTFS_shapes_editor_JS',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Documentos',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ docsaurius-test/sidebars.js
@@ -0,0 +1,10 @@
+module.exports = {
+  someSidebar: {
+    // Docusaurus: ['doc1', 'doc2', 'doc3'],
+    // Features: ['mdx'],
+    // Pacman: ['pacman'],
+    "Introducción": ['shape/shape_editor4'],
+    "Shape editor": ['shape/shape_editor', 'shape/shape_editor2', 'shape/shape_editor3']
+  },
+};
+1
